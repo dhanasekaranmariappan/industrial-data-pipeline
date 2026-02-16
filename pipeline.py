cbab64dad50163ea4c8dcd1c460b738adb5923f0
dhanasekaran-mariappan-22022000 <dhanasekaranmari2202@gmail.com>
2026-02-16 08:21:03 +0530

    Improved documentation and structure

--- pipeline.py
+++ pipeline.py
@@ -4,13 +4,23 @@ from src.transform import add_features
 from src.db import setup_database, insert_dataframe
 from src.analysis import fetch_data
 from src.visualize import plot_temperature
+import logging
 
+logging.basicConfig(level=logging.INFO)
+
+
+
+
+
+logging.info("Starting data pipeline...")
 setup_database()
 df = load_data("data/ai4i+2020+predictive+maintenance+dataset/ai4i2020.csv")
+logging.info("Loading dataset...")
 df = clean_data(df)
 df = add_features(df)
 insert_dataframe(df)
+logging.info("Writing to MySQL...")
 
 analytics_df = fetch_data()
 plot_temperature(analytics_df)
-
+logging.info("Pipeline completed successfully.")
